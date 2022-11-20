2c9c4f5a54898d49d298fbf393ccc467b27fd203
Ivo diogo Da cruz banha <ivda-cru@c1r9s1.42lisboa.com>
2022-11-20 20:57:28 +0000

    a big mistake found

--- client.c
+++ client.c
@@ -6,12 +6,60 @@
 /*   By: ivda-cru <ivda-cru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 19:44:58 by ivda-cru          #+#    #+#             */
-/*   Updated: 2022/11/20 13:08:37 by ivda-cru         ###   ########.fr       */
+/*   Updated: 2022/11/20 20:50:45 by ivda-cru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <string.h> // replace for the ft_atoi
+
+int		len(long nb)
+{
+	int		len;
+
+	len = 0;
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		len++;
+	}
+	while (nb > 0)
+	{
+		nb = nb / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int nb)
+{
+	char *str;
+	long	n;
+	int		i;
+
+	n = nb;
+	i = len(n);
+	if (!(str = (char*)malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	str[i--] = '\0';
+	if (n == 0)
+	{
+		str[0] = 48;
+		return (str);
+	}
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = n * -1;
+	}
+	while (n > 0)
+	{
+		str[i] = 48 + (n % 10);
+		n = n / 10;
+		i--;
+	}
+	return (str);
+}
 
 static void send_bit(char c, int pid)
 {
@@ -30,10 +78,31 @@ static void send_bit(char c, int pid)
     }     
 }
 
+static void send_str_len(char *str, int pid)
+{
+    int		shift;
+	char	bit;
+    int num;
+    
+    num = strlen(str);
+	shift = (sizeof(int) * 8) - 1;
+	while (shift >= 0)
+	{
+		bit = (num >> shift) & 1;
+        if(bit == 1)
+		    kill(pid, SIGUSR1);
+        else
+            kill(pid, SIGUSR2);
+		shift--;
+	}
+    
+    
+}
+
 static void send_string(char *str, int pid)
 {
-    int i;
-    
+    int i;    
+        
     i = 0;    
     while (str[i])
     {
@@ -66,19 +135,34 @@ int main (int argc, char **argv)
 {
     int pid;
     char *str;
-    
-    pid = atoi(argv[1]);    
-    str = strdup(argv[2]);   
+    if (argc == 3)
+    {
+        pid = atoi(argv[1]);    
+        str = strdup(argv[2]);   
+        
+    }
     signal(SIGUSR1, &bit_confirmation);
 	signal(SIGUSR2, &bit_confirmation_error);
 
+    send_str_len(str, pid);
     send_string(str, pid);
-   
-                
-       
-
-
     
 }
+
+/* int i;
+    int size_str;
+    char *str_len;
+    
+    i = 0;
+    size_str = (int)strlen(str);    
+    str_len = ft_itoa(size_str);
+
+    while (str_len[i])
+    {
+        send_bit(str_len[i], pid);
+        i++;
+    }
+    if (!str_len[i])
+        send_bit('\t', pid); */
 
 
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: ivda-cru <ivda-cru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 19:45:06 by ivda-cru          #+#    #+#             */
-/*   Updated: 2022/11/20 13:06:43 by ivda-cru         ###   ########.fr       */
+/*   Updated: 2022/11/20 20:54:30 by ivda-cru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,66 @@
 #include  <sys/types.h>
 #include  <signal.h> */
 
+//https://github.com/pvaladares/42cursus-02-minitalk/blob/main/src/server.c
+
+/* int lenght_of_str(char c)
+{
+    int i;
+    int atoi_len;
+    int x;
+    
+    
+    i = 0;    
+    while (c != '\t')
+    {
+        atoi_len = atoi(c);
+    }
+    shift
+    X |= 1 << (((sizeof(int) * 8) - 1) - Y);
+    x = x | 1 << (shift - Y);
+    x = 1 << (shift - Y) | x;
+} */
 
 static void signal_handler(int signum, siginfo_t *pid, void *palha)
 {
     static int i ;
     static unsigned char c;  
-    static pid_t			client_pid;  
+    static pid_t			client_pid;
+    static int str_len;  
+    static int y;
 
    
         (void)palha;
-        client_pid = pid->si_pid;    
-        if (signum == SIGUSR1)
+        client_pid = pid->si_pid;
+        //lenght_of_str(c);
+
+        if (y <= 32)
         {
-            c = 1 << i | c;
-            i++;
+            if (signum == SIGUSR1)
+                str_len = 1 << ((sizeof(int) * 8) - y) | str_len;
+            else
+                 str_len = 0 << ((sizeof(int) * 8) - y) | str_len;   
+            y++;        
         }
-        else if (signum == SIGUSR2)
-        {
-            c = 0 << i | c;
-            i++;            
-        }  
         
+
+        if (y == 31)
+            write(1, &str_len, sizeof(str_len));
+        
+
+        if(y > 32)
+        {
+            if (signum == SIGUSR1)
+            {
+                c = 1 << i | c;
+                i++;
+            }
+            else if (signum == SIGUSR2)
+            {
+                c = 0 << i | c;
+                i++;            
+            }             
+        }        
     
     if (i == 8)
     {   
