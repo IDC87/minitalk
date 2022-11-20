336eec4aedb4661636440246e74a30d8794024dc
IDC87 <ivo.diogo@gmail.com>
2022-11-20 13:29:37 +0000

    optimizations still needed

--- client.c
+++ client.c
@@ -6,60 +6,79 @@
 /*   By: ivda-cru <ivda-cru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 19:44:58 by ivda-cru          #+#    #+#             */
-/*   Updated: 2022/11/11 23:02:55 by ivda-cru         ###   ########.fr       */
+/*   Updated: 2022/11/20 13:08:37 by ivda-cru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include <string.h> // replace for the ft_atoi
 
-// client receives two arguments:
-    // the server PID
-    // the string to send
-
-void send_bit(char c, int pid)
+static void send_bit(char c, int pid)
 {
-    int i = 0;
+    __uint8_t i;
+
+    i = 0;    
     while(i < 8)
     {
-        if((c >> i++) & 1)
+        if((c >> i) & 1)
             kill(pid, SIGUSR1);
         else
             kill(pid, SIGUSR2); 
-            usleep(1000);  
-            
-    }
+           usleep(400); 
+     
+        i++;    
+    }     
+}
+
+static void send_string(char *str, int pid)
+{
+    int i;
     
+    i = 0;    
+    while (str[i])
+    {
+        send_bit(str[i], pid);
+        i++;
+    }
     
 }
 
+static void bit_confirmation(int signum)
+{   
+    if (signum == SIGUSR1)
+       // printf("BASED\n");  
+    while(signum != SIGUSR1) 
+        pause();   
+        
+}
+
+static void bit_confirmation_error(int signum)
+{     
+    
+    if (signum == SIGUSR2)
+        {
+            printf("\n\nNao aguentou a pujanca\n\n");
+            exit(0);
+        }
+}
 
 int main (int argc, char **argv)
 {
     int pid;
-    int ret;
     char *str;
-    int i;
-
-    //i = 01101001
-    i = 0;
     
     pid = atoi(argv[1]);    
-    str = argv[2];
+    str = strdup(argv[2]);   
+    signal(SIGUSR1, &bit_confirmation);
+	signal(SIGUSR2, &bit_confirmation_error);
 
-    printf("ARGS \n%d\n%s\n", pid, str);
+    send_string(str, pid);
+   
+                
+       
 
-    //send_bit(str[0], pid);
-    
-    while(str[i])
-    {
-        send_bit(str[i], pid);
-        i++;
-        //usleep(300);
-    }
-
-    /* while(1)
-        pause();
- */
 
     
 }
+
+
--- server.c
+++ server.c
@@ -6,56 +6,82 @@
 /*   By: ivda-cru <ivda-cru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 19:45:06 by ivda-cru          #+#    #+#             */
-/*   Updated: 2022/11/11 23:06:11 by ivda-cru         ###   ########.fr       */
+/*   Updated: 2022/11/20 13:06:43 by ivda-cru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+//#define _GNU_SOURCE
 
-#include  <stdio.h>
+//c = ((signum == SIGUSR1) << i) | c; 
+/* #include  <stdio.h>
 #include  <sys/types.h>
-#include  <signal.h>
-#include  <sys/ipc.h>
-#include  <sys/shm.h>
+#include  <signal.h> */
 
-static void signal_handler(int signum)
+
+static void signal_handler(int signum, siginfo_t *pid, void *palha)
 {
     static int i ;
-    static char c;    
+    static unsigned char c;  
+    static pid_t			client_pid;  
 
-    if (i < 8)
-    {
-        c = ((signum == SIGUSR1) << i) | c;
+   
+        (void)palha;
+        client_pid = pid->si_pid;    
+        if (signum == SIGUSR1)
+        {
+            c = 1 << i | c;
+            i++;
+        }
+        else if (signum == SIGUSR2)
+        {
+            c = 0 << i | c;
+            i++;            
+        }  
         
-        i++;       
-        
-    }
     
     if (i == 8)
-    {
+    {   
+        if (!c)
+        {
+            kill(client_pid, SIGUSR2); 
+            printf("\n\nerror comes from here!\n\n");    
+            exit(0);
+        }  
+        
         write(1, &c, 1);
         i = 0; 
-        c = 0;
-        
+        c = 0;         
+            
     } 
-      
+    kill(client_pid, SIGUSR1);
     
-}
-
+    
+}  
 
 int main(void)
 {
     int pid;
-    //struct sigaction test;
-
+   struct sigaction sig;
+     
     pid = getpid();
-    printf("%d\n", pid );
-    signal(SIGUSR1, &signal_handler);
-    usleep(500); 
-    signal(SIGUSR2, &signal_handler);
-
+    printf("%d\n", pid);
+    sig.sa_flags = SA_SIGINFO;   
+    sig.sa_sigaction = &signal_handler;
+    sigemptyset(&sig.sa_mask);
     while(1)
-        pause();
+    {
+            if(!SIGUSR1 && !SIGUSR2)
+                pause();                
+            else
+            {               
+                sigaction(SIGUSR1, &sig, NULL);
+                sigaction(SIGUSR2, &sig, NULL); 
+                     
+            }
+          
+    }
+
     
    return (0);
 }
